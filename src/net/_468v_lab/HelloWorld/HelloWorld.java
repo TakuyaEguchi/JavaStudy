99cad7e5a75b419c6bfd3ea24e415b5b542e9873
TakuyaEguchi <kiyo.468v@gmail.com>
2016-06-22 22:54:33 +0900

    first commit

---
+++ src/net/_468v_lab/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package net._468v_lab.HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld");
+
+	}
+
+}
