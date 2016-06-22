c307f2b255442d5c018a89f656c16e0760ea740f
TakuyaEguchi <kiyo.468v@gmail.com>
2016-06-22 23:23:23 +0900

    add main class

---
+++ src/net/_468v_lab/Iterator/Main.java
@@ -0,0 +1,18 @@
+package net._468v_lab.Iterator;
+
+/**
+ * Main class
+ * @author http://www.techscore.com/tech/DesignPattern/Iterator/Iterator2.html/
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Teacher you = new MyTeacher();
+		you.createStudentList();
+		you.callStudents();
+
+	}
+
+}
