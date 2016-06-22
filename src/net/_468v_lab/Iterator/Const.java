f79ac20a8eab5342e174484fd131ff932d6e1898
TakuyaEguchi <kiyo.468v@gmail.com>
2016-06-22 23:26:29 +0900

    add Const class

---
+++ src/net/_468v_lab/Iterator/Const.java
@@ -0,0 +1,17 @@
+package net._468v_lab.Iterator;
+
+/**
+ * Sex Constant class
+ * @author TakuyaEguchi
+ *
+ */
+public class Const {
+	
+	// privateコンストラクタでインスタンス生成を抑止
+	private Const(){}
+	
+	// 定数
+	public static final int BOY = 0;
+	public static final int GIRL = 1;
+
+}
