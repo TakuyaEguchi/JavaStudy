a8665f71cf576fb2db03a4c08220ca4258495d4c
TakuyaEguchi <kiyo.468v@gmail.com>
2016-06-26 20:45:09 +0900

    create Hero instance

--- src/net/_468v_lab/Intro/Main.java
+++ src/net/_468v_lab/Intro/Main.java
@@ -14,6 +14,20 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// 1.勇者を作成
+		Hero minato = new Hero();
+		
+		// 2.フィールドに初期値を設定
+		minato.name = "ミナト";
+		minato.hp = 100;
+		System.out.println("勇者" + minato.name + "が誕生した");
+		
+		// 3.勇者メソッドを呼び出し
+		minato.sit(5);
+		minato.slip();
+		minato.sit(25);
+		minato.run();
 
 	}
 
